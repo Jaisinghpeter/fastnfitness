271f3ddab323a0dc0a6d0c1cf75793c487bdb9ec
jai-9535 <jaisingh.d@zohocorp.com>
2020-03-08 10:08:53 +0530

    Commited the 2nd level of chages

--- app/src/main/java/com/easyfitness/SettingsFragment.java
+++ app/src/main/java/com/easyfitness/SettingsFragment.java
@@ -2,6 +2,9 @@ package com.easyfitness;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.preference.ListPreference;
@@ -29,74 +32,18 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        mActivity = (MainActivity) getActivity();
-//
-//        //addPreferencesFromResource(R.xml.settings);
-//
-//        Preference myPref = findPreference("prefShowMP3");
-//        myPref.setOnPreferenceChangeListener((preference, newValue) -> {
-//            if (newValue instanceof Boolean) {
-//                Boolean boolVal = (Boolean) newValue;
-//                mActivity.showMP3Toolbar(boolVal);
-//            }
-//
-//            return true;
-//        });
-//
-//        Preference myPref2 = findPreference("defaultUnit");
-//        myPref2.setOnPreferenceChangeListener((preference, newValue) -> {
-//            ListPreference listPreference = (ListPreference) preference;
-//            if (newValue instanceof String) {
-//                //find the index of changed value in settings.
-//                updateSummary(listPreference, (String) newValue, getString(R.string.pref_preferredUnitSummary));
-//            }
-//
-//            return true;
-//        });
-//
-//        Preference myPref3 = findPreference("defaultDistanceUnit");
-//        myPref3.setOnPreferenceChangeListener((preference, newValue) -> {
-//            ListPreference listPreference = (ListPreference) preference;
-//            if (newValue instanceof String) {
-//                //find the index of changed value in settings.
-//                updateSummary(listPreference, (String) newValue, getString(R.string.pref_preferredUnitSummary));
-//            }
-//
-//            return true;
-//        });
-//
-//        Preference dayNightModePref = findPreference("dayNightAuto");
-//        dayNightModePref.setOnPreferenceChangeListener((preference, newValue) -> {
-//            ListPreference listPreference = (ListPreference) preference;
-//            if (newValue instanceof String) {
-//                updateSummary(listPreference, (String) newValue, "");
-//            }
-//
-//            return true;
-//        });
     }
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String param) {
-        //addPreferencesFromResource(R.xml.settings);
-//        setPreferencesFromResource(R.xml.settings2, param);
-//
-//        SharedPreferences sharedPreferences;
-//        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-//
-//        ListPreference myPref2 = (ListPreference) findPreference("defaultUnit");
-//        String boolVal = sharedPreferences.getString("defaultUnit", "0");
-//        updateSummary(myPref2, boolVal, getString(R.string.pref_preferredUnitSummary));
-//
-//        ListPreference myPref3 = (ListPreference) findPreference("defaultDistanceUnit");
-//        String boolVal3 = sharedPreferences.getString("defaultDistanceUnit", "0");
-//        updateSummary(myPref3, boolVal3, getString(R.string.pref_preferredUnitSummary));
-//
-//        ListPreference dayNightModePref = (ListPreference) findPreference("dayNightAuto");
-//        String dayNightValue = sharedPreferences.getString("dayNightAuto", "2");
-//        updateSummary(dayNightModePref, dayNightValue, "");
 
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.tab_about, container, false);
+        return view;
     }
 
     private void updateSummary(ListPreference pref, String val, String prefix) {
